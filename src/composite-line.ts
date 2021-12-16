2813cef9e8c9b1a9bf66a01528ad25eda27772c5
j- <j@skeoh.com>
2021-12-16 23:16:53 +1100

    Output relative path when downloading

--- src/composite-line.ts
+++ src/composite-line.ts
@@ -1,4 +1,8 @@
-import { ContiguousLine, buildContiguousLinePathDefinitionAbsolute } from './contiguous-line';
+import {
+  buildContiguousLinePathDefinitionAbsolute,
+  buildContiguousLinePathDefinitionRelative,
+  ContiguousLine,
+} from './contiguous-line';
 
 /** A composite line is a collection of contiguous lines of the same style. */
 export type CompositeLine = ContiguousLine[];
@@ -6,5 +10,11 @@ export type CompositeLine = ContiguousLine[];
 export const buildCompositeLinePathDefinitionAbsolute = (lines: CompositeLine): string => (
   lines
     .map(buildContiguousLinePathDefinitionAbsolute)
+    .join(' ')
+);
+
+export const buildCompositeLinePathDefinitionRelative = (lines: CompositeLine): string => (
+  lines
+    .map(buildContiguousLinePathDefinitionRelative)
     .join(' ')
 );
