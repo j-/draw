0e1fb06feef907de6df9828ef6b2685bbd469bb3
j- <j@skeoh.com>
2022-01-27 21:52:46 +1100

    Simplify cursor radius SVG

--- src/cursor.ts
+++ src/cursor.ts
@@ -1,4 +1,4 @@
 export const generateCursorProperty = (size: number, color: string = 'black') => {
   const radius = size / 2;
-  return `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='-50 -50 100 100' width='${size}' height='${size}'%3E%3Ccircle cx='0' cy='0' r='50' fill='${encodeURIComponent(color)}' /%3E%3C/svg%3E") ${radius} ${radius}, crosshair`;
+  return `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 2 2' width='${size}' height='${size}'%3E%3Ccircle cx='1' cy='1' r='1' fill='${encodeURIComponent(color)}' /%3E%3C/svg%3E") ${radius} ${radius}, crosshair`;
 };
